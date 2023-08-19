f32f21f470c33e9595dc0513521d4065f36fe45d
thexdev <thexdev@pop-os.localdomain>
2023-08-19 09:32:36 -0300

    feat : add like on comments

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 import { ThumbsUp, Trash } from "@phosphor-icons/react"
 
 export const Comment = ({content, onDeleteComment}) => {
+    const [likeCount, setLikeCount] = useState(0);
+
+    const handleLikeComment = () => {
+        setLikeCount((state) => state + 1)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar
@@ -31,9 +38,9 @@ export const Comment = ({content, onDeleteComment}) => {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
